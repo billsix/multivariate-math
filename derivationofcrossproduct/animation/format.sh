60e4cd457c06aab4db218027486d429056e2c714
William Emerison Six <billsix@gmail.com>
2023-07-09 16:03:53 -0500

    made format work on macos

--- derivationofcrossproduct/animation/format.sh
+++ derivationofcrossproduct/animation/format.sh
@@ -1,5 +1,5 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
-for f in `find ./src/ -name "*.py"`; do autoflake8 --in-place $f; done
-for f in `find ./src/ -name "*.py"`; do black -l 120 $f; done
-for f in `find ./src/ -name "*.py"`; do removestar -i $f; done
+for f in $(find ./src/ -name "*.py"); do autoflake8 --in-place $f; done
+for f in $(find ./src/ -name "*.py"); do black -l 120 $f; done
+for f in $(find ./src/ -name "*.py"); do removestar -i $f; done
