a207a0697c82758303feca5bf7f617c198a269df
William Emerison Six <billsix@gmail.com>
2026-07-07 22:33:46 -0400

    updated emacs packages

---
+++ entrypoint/dotfiles/.emacs.d/elpa/dracula-theme-20260701.2051/dracula-theme-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dracula-theme" "20260701.2051"
+  "Dracula Theme."
+  '((emacs "24.3"))
+  :url "https://github.com/dracula/emacs"
+  :commit "1acd97258258415f6a43c332475fbdcd7ceb9274"
+  :revdesc "1acd97258258"
+  :maintainers '(("tienne Deparis" . "etienne@depar.is")))
---
+++ entrypoint/dotfiles/.emacs.d/elpa/org-modern-20260707.1016/org-modern-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "org-modern" "20260707.1016"
+  "Modern looks for Org."
+  '((emacs  "29.1")
+    (org    "9.6")
+    (compat "31"))
+  :url "https://github.com/minad/org-modern"
+  :commit "d41bedbab849745bd10e300b8d93a17bc78a5ad6"
+  :revdesc "d41bedbab849"
+  :keywords '("outlines" "hypermedia" "text")
+  :authors '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
